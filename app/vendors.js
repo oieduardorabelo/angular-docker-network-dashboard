a8305907b23eea525aa5f3295bfc01a8cc9ba19b
Eduardo Rabelo <oieduardorabelo@gmail.com>
2015-04-17 00:59:52 +1200

    third-party dependencies

---
+++ app/vendors.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  // Third-party libraries
+  require('bootstrap');
+  require('holderjs?global=window');
+
+  // Style sheets
+  require('./assets/less/main.less');
+};
