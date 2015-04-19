660db93923f360cd80e994fdf9e3169c909ca21b
Eduardo Rabelo <oieduardorabelo@gmail.com>
2015-04-19 15:08:42 +1200

    adding ui-router and routes file

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const angular = require('angular')
-const ngModule = angular.module('NetworkDashboardApp', [])
+const ngModule = angular.module('NetworkDashboardApp', [require('angular-ui-router')])
 
 require('./vendors')()
+require('./angular.routes')(ngModule)
 require('./directives')(ngModule)
