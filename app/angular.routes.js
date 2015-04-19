23e27a6332029c664c4d531e32e3e0663aeb23fa
Eduardo Rabelo <oieduardorabelo@gmail.com>
2015-04-19 15:09:17 +1200

    routes file

---
+++ app/angular.routes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+export default (ngModule) => {
+  ngModule.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/dashboard')
+
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        template: '<dashboard></dashboard>'
+      })
+      .state('settings', {
+        url: '/settings',
+        template: '<settings></settings>'
+      })
+      .state('profile', {
+        url: '/profile',
+        template: '<profile></profile>'
+      })
+      .state('help', {
+        url: '/help',
+        template: '<help></help>'
+      })
+  })
+}
