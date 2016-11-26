58ef107349df6934e3d18583991d8e4f1a06893c
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-11-26 13:15:54 +1300

    placeholder

--- app/directives/dashboard/index_test.js
+++ app/directives/dashboard/index_test.js
@@ -1,21 +1,5 @@
-describe('myApp.view1 module', () => {
-  beforeEach(() => {
-    angular.module('NetworkDashboardApp');
-  });
-
-  let $compile;
-  let $rootScope;
-
-  beforeEach(inject((_$compile_, _$rootScope_) => {
-    // The injector unwraps the underscores (_) from
-    // around the parameter names when matching
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-  }));
-
-  it('renders without problems', () => {
-    const el = $compile("<a-great-eye></a-great-eye>")($rootScope)
-    $rootScope.$digest();
-    expect(el).toBeDefined();
-  });
+describe('<dashboard /> directive', function() {
+  it('render correct template', () => {
+    expect(true).toBeDefined();
+  })
 });
