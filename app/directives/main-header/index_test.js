85635e13f7729a1945d53e2eec76067748e9c197
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-11-28 10:27:06 +1300

    more tests

---
+++ app/directives/main-header/index_test.js
@@ -0,0 +1,25 @@
+describe('Unit testing <main-footer /> directive', () => {
+  let $compile;
+  let $rootScope;
+
+  // Load the myApp module, which contains the directive
+  beforeEach(angular.mock.module('NetworkDashboardApp'));
+
+  // Store references to $rootScope and $compile
+  // so they are available to all tests in this describe block
+  beforeEach(inject((_$compile_, _$rootScope_) => {
+    // The injector unwraps the underscores (_) from around
+    // the parameter names when matching
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('render correct template', () => {
+    // Compile a piece of HTML containing the directive
+    const element = $compile('<main-header></main-header>')($rootScope);
+    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+    $rootScope.$digest();
+    // Check that the compiled element contains the templated content
+    expect(element.find('.navbar.navbar-inverse.navbar-fixed-top')).toBeDefined();
+  });
+});
