89f11e6d3a0735586b00b4ef736a3a479dbc23fc
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-12-13 00:04:20 +1300

    removing karma files

--- karma.setup.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import mocks from 'angular-mocks';
-
-import * as main from './app/index';
-
-//make sure you have your directory and regex test set correctly!
-let context = require.context('./app', true, /test\.js$/);
-context.keys().forEach(context);
