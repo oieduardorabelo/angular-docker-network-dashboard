57aafd84a7561c92d5e4c8e04ad8f60419a80a2b
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-04-03 18:55:29 +1200

    webpack v2 config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodePath = require('path');
 const path = dir => nodePath.join(__dirname, dir);
 
 module.exports = {
-  devtools: 'inline-source-map',
+  devtool: 'inline-source-map',
   context: path('/app'),
   entry: './index.js',
   output: {
@@ -20,11 +20,11 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-      { test: /\.html$/, loader: 'raw', exclude: /node_modules/ },
-      { test: /\.less$/, loader: 'style!css!less', exclude: /node_modules/ },
-      { test: /\.(woff|woff2)$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)$/, loader: 'file' },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.html$/, use: 'raw-loader', exclude: /node_modules/ },
+      { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'], exclude: /node_modules/ },
+      { test: /\.(woff|woff2)$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)$/, use: 'file-loader' },
     ],
   },
 };
